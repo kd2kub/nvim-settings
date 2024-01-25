67d018f5020817dd65423ea34f713d76860e7c83
kd2kub <kd2kub@debian-Bookworm-build>
2024-01-24 22:36:15 -0500

    fixed issues with harpoon and getting it to load in other directories

--- nvim/lua/kd2kub/init.lua
+++ nvim/lua/kd2kub/init.lua
@@ -1,5 +1,8 @@
 require("kd2kub.set")
 require("kd2kub.remap")
-print("Loading kd2kub/ stuff")
+require("kd2kub.harpoon")
+require("kd2kub.packer")
+require("kd2kub.undotree")
+print("Loading kd2kub / stuff")
 --
 --need map leader in here
